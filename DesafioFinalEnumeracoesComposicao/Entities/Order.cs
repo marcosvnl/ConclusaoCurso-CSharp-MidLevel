d37ca5445c5ea9114d8d98295c79d6a882c790b0
JIDD Sistemas - Dev <JIDD Sistemas - Dev@DESKTOP-AI7K7N2>
2021-03-03 10:40:59 -0300

    Primeiro Commit

---
+++ DesafioFinalEnumeracoesComposicao/Entities/Order.cs
@@ -0,0 +1,71 @@
+﻿using DesafioFinalEnumeracoesComposicao.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace DesafioFinalEnumeracoesComposicao.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime date, OrderStatus status, Client client)
+        {
+            Date = date;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem items)
+        {
+            Items.Add(items);
+        }
+
+        public void RemoveItem(OrderItem items)
+        {
+            Items.Remove(items);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem items in Items)
+            {
+                sum += items.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY");
+            sb.Append("Order moment: ");
+            sb.AppendLine(Date.ToString());
+            sb.AppendLine("Order status: " + Status);
+            sb.AppendLine("Client: " + Client.Name + " " + Client.BirthDate.ToString("(dd/MM/yyyy)") + " - " + Client.Email);
+            sb.AppendLine("Order items:");
+            foreach (OrderItem items in Items)
+            {
+                sb.Append(items.Product.Name + ", ");
+                sb.Append(items.Product.Price + ", ");
+                sb.Append("Quantity: " + items.Quantity + ", ");
+                sb.AppendLine("Subtotal: " + items.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+            Console.WriteLine();
+            sb.AppendLine("Total prince: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ DesafioFinalEnumeracoesComposicao/Program.cs
@@ -0,0 +1,52 @@
+﻿using DesafioFinalEnumeracoesComposicao.Entities;
+using DesafioFinalEnumeracoesComposicao.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace DesafioFinalEnumeracoesComposicao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Client data
+            Console.WriteLine("Enter clint data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("E-mail: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            //Order data
+            Console.WriteLine("Enter order data:");
+            DateTime moment = DateTime.Now;
+            Console.WriteLine();
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Client client = new Client(name, email, birthdate);
+            Order order = new Order(moment, status, client);
+
+            Console.Write("How many items to this order: ");
+            int n = int.Parse(Console.ReadLine());
+            //Items
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data");
+                Console.Write("Product Name: ");
+                string nameProduct = Console.ReadLine();
+                Console.Write("Product price: ");
+                double princeProduct = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int nProduct = int.Parse(Console.ReadLine());
+
+                Product product = new Product(nameProduct, princeProduct);
+                OrderItem items = new OrderItem(nProduct, princeProduct, product);
+                order.AddItem(items);
+            }
+
+            Console.WriteLine(order);
+        }
+    }
+}
